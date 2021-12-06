b2f1c5b495ab33fcc455c262799f774ac8c4329d
Guilherme <guilherme.trombini@labsit.io>
2021-12-05 23:18:08 -0300

    removing comments

--- src/app/pokedashSlice.ts
+++ src/app/pokedashSlice.ts
@@ -65,12 +65,6 @@ export const pokemonSlice = createSlice({
 
 export const { add, remove, editPokemon } = pokemonSlice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const pokemons = (state: RootState) => state.pokemons;
-
-// We can also write thunks by hand, which may contain both sync and async logic.
-// Here's an example of conditionally dispatching actions based on current state.
 
 export default pokemonSlice.reducer;
